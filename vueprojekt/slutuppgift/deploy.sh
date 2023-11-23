9a579a8c11a8cc873c20e5e9f3b9a35f97dbd3ef
unknown <kalle.r.persson@gmail.com>
2023-11-23 13:08:54 +0100

    2

--- vueprojekt/slutuppgift/deploy.sh
+++ vueprojekt/slutuppgift/deploy.sh
@@ -10,14 +10,14 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'www.example.com' > CNAME
+#echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+#git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:BrokenLegs/fordjupning-programmering.git main:gh-pages
